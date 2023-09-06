d03ba8b39c41e73c3b2559a1131339785a79fb92
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2023-09-06 22:48:18 +0300

    d

--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -60,8 +60,8 @@ const SingleMoviePage = () => {
   // console.log(title);
   const [searchParams] = useSearchParams();
 
-  const title = searchParams.get("title");
-  // const title = id;
+  // const title = searchParams.get("title");
+  const title = `${id}TITLE`;
 
   console.log(`title`, title);
   return (
