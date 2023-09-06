fb9c763cf7cffdc5657bb5b271c3a7bb949d9d8c
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2023-09-06 21:30:51 +0300

    fix

--- src/pages/SingleMoviePage/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage/SingleMoviePage.jsx
@@ -59,43 +59,14 @@ const SingleMoviePage = () => {
 
   return (
     <>
-      {/* <Helmet>
-        <title>{title ? title : "some"}</title>
-      </Helmet> */}
       <Helmet>
-        <title>{title} | Greether</title>
+        <title>{`${title} | Greether`}</title>
         <meta name="title" content={title} />
         <meta
           name="description"
           content={`GE helps women travel safer by connecting them to local Greeters they can book to navigate ${title}  'Mexico city', ${title}  'Mexico'
          in an authentic and safe way. We provide you with your local best friend!`}
         />
-        {/* { title ? <link rel="canonical" href={ title } /> : null }
-        { title ? <link rel="image_src" href={ https://image.tmdb.org/t/p/w600_and_h900_bestv2 } }
-          
-        <meta property="og:site_name" content="Greether" />
-        <meta property="og:title" content={Greeter Booking ${searchParams?.country} | Greether} />
-        <meta property="og:description" content={`Greether helps women travel safer by connecting them to local Greeters they can book to navigate ${
-          searchParams?.city  'Mexico city'
-        }, ${
-          searchParams?.country  'Mexico'
-        } in an authentic and safe way. We provide you with your local best friend!`} />
-        { canonical ? <meta property="og:url" content={canonical} /> : null }
-        <meta property="og:locale" content={locale} />
-        <meta property="og:type" content="website" />
-        { image ? <meta property="og:image" content={image} /> : null }
-        
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={Greeter Booking ${searchParams?.country} | Greether} />
-        <meta name="twitter:description" content={`Greether helps women travel safer by connecting them to local Greeters they can book to navigate ${
-            searchParams?.city  'Mexico city'
-          }, ${
-            searchParams?.country  'Mexico'
-          } in an authentic and safe way. We provide you with your local best friend!`} /> */}
-
-        {/* <meta name="twitter:site" content="@GreetherOfficial" /> */}
-
-        {/* { image ? <meta name="twitter:image" content={image} /> : null } */}
       </Helmet>
       <div className={s.wrapper}>
         <button className={s.btnBack} type="button" onClick={goBack}>
